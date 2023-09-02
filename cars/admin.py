a11e1986317a7413d9a1523a99b6193e7138c245
Rigers Collaku <rigers.collaku@icloud.com>
2023-09-03 00:25:47 +0200

    some small changes

--- cars/admin.py
+++ cars/admin.py
@@ -7,24 +7,34 @@ class ReservationAdmin(admin.ModelAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if obj is None:  # Creating a new reservation
+        if obj is None:
             form.base_fields['user'].initial = request.user
             form.base_fields['user'].widget.attrs['readonly'] = True
         return form
 
 
 class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', )
+    list_display = ('email', 'name',)
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if obj is None:  # Creating a new reservation
+        if obj is None:
             form.base_fields['email'].initial = request.email
             form.base_fields['email'].widget.attrs['readonly'] = True
         return form
 
 
-admin.site.register(Reservation, ReservationAdmin)
-admin.site.register(Car)
-admin.site.register(ContactMessage, ContactMessageAdmin)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('mark', 'model')
 
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if obj is None:  # Creating a new reservation
+            form.base_fields['model'].initial = request.model
+            form.base_fields['model'].widget.attrs['readonly'] = True
+        return form
+
+
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(ContactMessage, ContactMessageAdmin)
